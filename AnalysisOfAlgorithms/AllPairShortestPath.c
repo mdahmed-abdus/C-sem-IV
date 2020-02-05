fa8604613703a61e35ab2622aa801e37da6f890f
Mohd Ahmed Abdus <49087790+mdahmed-abdus@users.noreply.github.com>
2020-02-05 22:20:30 +0530

    Create AllPairShortestPath.c

---
+++ AnalysisOfAlgorithms/AllPairShortestPath.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+#define ROW 20
+#define COLUMN 20
+
+int min(int no1, int no2);
+void allPair(int path[ROW][COLUMN], int cost[ROW][COLUMN], int noOfNodes);
+
+int main()
+{
+    int path[ROW][COLUMN], cost[ROW][COLUMN], noOfNodes;
+    int i, j;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &noOfNodes);
+
+    printf("If there is no edge then enter 1000\n");
+    for (i = 1; i <= noOfNodes; i++)
+        for (j = 1; j <= noOfNodes; j++)
+        {
+            if (i != j)
+            {
+                printf("Enter the cost of edge [%d, %d]: ", i, j);
+                scanf("%d", &cost[i][j]);
+            }
+            else
+                cost[i][j] = 0;
+        }
+
+    allPair(path, cost, noOfNodes);
+
+    printf("Shortest path is:\n\t");
+    for (i = 1; i <= noOfNodes; i++)
+        printf("%d\t", i);
+    printf("\n");
+    for (i = 1; i <= noOfNodes; i++)
+    {
+        printf("\n%d\t", i);
+        for (j = 1; j <= noOfNodes; j++)
+            printf("%d\t", path[i][j]);
+        printf("\n");
+    }
+
+    printf("\n");
+    return 0;
+}
+
+int min(int no1, int no2)
+{
+    if (no1 < no2)
+        return no1;
+    return no2;
+}
+
+void allPair(int path[ROW][COLUMN], int cost[ROW][COLUMN], int noOfNodes)
+{
+    int i, j, k;
+
+    for (i = 1; i <= noOfNodes; i++)
+        for (j = 1; j <= noOfNodes; j++)
+            path[i][j] = cost[i][j];
+
+    for (k = 1; k <= noOfNodes; k++)
+        for (i = 1; i <= noOfNodes; i++)
+            for (j = 1; j <= noOfNodes; j++)
+                path[i][j] = min(path[i][j], path[i][k] + path[k][j]);
+}
