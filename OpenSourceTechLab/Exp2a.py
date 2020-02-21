9eba5aaca717215a43101c3126d425bb0fe8fccd
Mohd Ahmed Abdus <49087790+mdahmed-abdus@users.noreply.github.com>
2020-02-21 21:45:50 +0530

    Increased efficiency.

--- OpenSourceTechLab/Exp2a.py
+++ OpenSourceTechLab/Exp2a.py
@@ -4,7 +4,7 @@ limit = int(input("Enter the upper limit: "))
 
 print("Prime numbers are:")
 for num in range(1, limit + 1):
-    for div in range(2, num):
+    for div in range(2, int(num / 2)):
         if num % div == 0:
             break
     else:
