3cb7d1268007e1cba6965daf9951403605dd186c
Mohd Ahmed Abdus <49087790+mdahmed-abdus@users.noreply.github.com>
2020-02-26 23:42:37 +0530

    Converted def to lambda.

--- OpenSourceTechLab/exp3b.py
+++ OpenSourceTechLab/exp3b.py
@@ -1,6 +1,5 @@
 # lambda function to find power of a number
-def power(number, raise_to): return number ** raise_to
-
+power = lambda number, raise_to: number ** raise_to
 
 number = int(input("Enter the number to find its power: "))
 raise_to = int(input("Enter the power: "))
